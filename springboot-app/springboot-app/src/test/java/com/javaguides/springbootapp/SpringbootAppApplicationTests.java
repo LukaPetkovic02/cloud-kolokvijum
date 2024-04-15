788b12c8fb8a12672f9bd3d05c7ba060f83ceea2
Luka Petković <lukapetkovicns21@gmail.com>
2024-04-15 22:33:54 +0200

    small fix

--- springboot-app/springboot-app/src/test/java/com/javaguides/springbootapp/SpringbootAppApplicationTests.java
+++ springboot-app/springboot-app/src/test/java/com/javaguides/springbootapp/SpringbootAppApplicationTests.java
@@ -3,11 +3,11 @@ package com.javaguides.springbootapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SpringbootAppApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class SpringbootAppApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
